9fab8b401177bcc6d5fd55283b74f5181058eec2
Nick <20791144+4E-69-63-6B-20-42-6F-73@users.noreply.github.com>
2022-02-17 11:10:12 +0100

    Initial commit

---
+++ Avengers/KmerGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Avengers
+{
+    public class KmerGenerator
+    {
+        public IEnumerable<string> Generate(string read, int k)
+        {
+            for (int i = 0; i < read.Length - k; i++)
+            {
+                yield return read.Substring(i, k);
+            }
+
+        }
+    }
+}
